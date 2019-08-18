0fd6365325c854dc6c80c5cce655e97296237d5b
uekyorf <uekyorf@gmail.com>
2019-08-18 12:24:25 +0900

    Add: postを受け取るメソッドを追加

--- src/main/java/com/example/demo/WebApiController.java
+++ src/main/java/com/example/demo/WebApiController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @RestController
@@ -24,6 +26,11 @@ public class WebApiController {
     @RequestMapping("/test")
     private String testRequestParam( @RequestParam() String param ) {
         return "受け取ったパラメータ：" + param;
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    private String testRequestBody( @RequestBody String body ) {
+        return "受け取ったリクエストボディ：" + body;
     }
 
 }
